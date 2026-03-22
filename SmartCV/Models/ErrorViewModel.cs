b7bf77ef13cf28e954be4d671de7011c70b3fab0
Lê Triệu Văn <vanlt322@uef.edu.vn>
2026-03-22 15:12:34 +0700

    start project

---
+++ SmartCV/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmartCV.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
